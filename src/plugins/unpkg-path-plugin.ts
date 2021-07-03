dfaf3a3b8f0b50165c452f7cba4fa7a7df0efe68
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-03 12:29:21 -0600

    used URL constructor to retrieve npm package

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,6 +10,13 @@ export const unpkgPathPlugin = () => {
         if(args.path === 'index.js') {
           return { path: args.path, namespace: 'a', };
       } 
+      
+      if(args.path.includes("./") || args.path.includes("../")) {
+        return {
+          namespace: "a",
+          path: new URL(args.path, args.importer + "/").href
+        }
+      }
 
       return {
         namespace: "a",
@@ -30,7 +37,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
