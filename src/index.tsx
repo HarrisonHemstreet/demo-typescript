bec31d27547e6a84a09841c7602c2676bef40882
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-04 16:00:16 -0600

    furthur refactoring- now I can display more CodeCells within index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,16 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from "./components/code-editor";
-import Preview from './components/Preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-	const [code, setCode] = useState('');
-	const [input, setInput] = useState("");
-
-
-	const onClick = async () => {
-		const output = await bundle(input);
-		setCode(output);
-	};
-
 
 	return (
 		<div>
-			<CodeEditor 
-				initialValue="const a = 1;"
-				onChange={(value) => setInput(value)}
-			/>
-			<div>
-				<button onClick={onClick}>Submit</button>
-			</div>
-			<Preview code={code} />
+			<CodeCell />
+			<CodeCell />
+			<CodeCell />
+			<CodeCell />
+			<CodeCell />
 		</div>
 	)
 };
