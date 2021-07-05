19fc70676555b02c5ae795d31c0298c117548b53
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-05 17:34:04 -0600

    fixed up button issue

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -37,14 +37,14 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
-
-      if (targetIndex === 0 || targetIndex > state.order.length - 1) {
+  
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
         return state;
       }
-
+  
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
-
+  
       return state;
 
     case ActionType.INSERT_CELL_BEFORE:
