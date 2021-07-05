6a85719f363af0e53251ac9a4f11ea4fde41c39d
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-05 13:08:53 -0600

    defined direction

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,12 @@
 import { ActionType } from "../action-types";
-import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellBeforeAction } from "../actions";
+import { 
+		Action, 
+		UpdateCellAction, 
+		DeleteCellAction, 
+		MoveCellAction, 
+		InsertCellBeforeAction, 
+		Direction,
+		} from "../actions";
 import { CellTypes } from "../cell";
 
 
@@ -21,7 +28,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
 	};
 };
 
-export const moveCell = (id: string, direction: "up" | "down"): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
 	return {
 		type: ActionType.MOVE_CELL,
 		payload: {
