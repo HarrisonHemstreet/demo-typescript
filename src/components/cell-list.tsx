a550efbab6079f7b7ba6cd16ab678c86bd28d8a7
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-05 15:03:04 -0600

    now can render out list of cells. need to add cells to test next though

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,9 +1,12 @@
 import { useTypedSelector } from "../hooks/use-type-selector";
+import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
 	const cells = useTypedSelector(( { cells: { order, data } }) => order.map((id) => data[id]));
 
-	return <div>CELL LIST</div>;
+	const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell} />)
+
+	return <div>{ renderedCells }</div>;
 };
 
 export default CellList;
