40334cbcfd1973625c10f1b5554df811f75d20c2
HarrisonHemstreet <harrisonhemstreet@gmail.com>
2021-07-05 20:13:50 -0600

    fixed the loader and the loader background!

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,52 +1,66 @@
+import './code-cell.css';
 import { useEffect } from 'react';
-import CodeEditor from "./code-editor";
+import CodeEditor from './code-editor';
 import Preview from './Preview';
-import Resizable from "./resizable";
-import { Cell } from "../state";
-import { useActions } from "../hooks/use-actions";
-import { useTypedSelector } from "../hooks/use-type-selector";
+import Resizable from './resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
+import { useTypedSelector } from '../hooks/use-type-selector';
 
 interface CodeCellProps {
-	cell: Cell
+  cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-	const { updateCell, createBundle } = useActions();
-	const bundle = useTypedSelector((state) => state.bundles[cell.id])
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
-	useEffect(() => {
-		if(!bundle) {
-			createBundle(cell.id, cell.content)
-			return;
-		}
-		const timer = setTimeout(async () => {
-			createBundle(cell.id, cell.content)
-		}, 750);
+  useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
 
-		return () => {
-			clearTimeout(timer);
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [cell.content, cell.id, createBundle]);
+    const timer = setTimeout(async () => {
+      createBundle(cell.id, cell.content);
+    }, 750);
 
-	return (
-		<Resizable direction="vertical">
-			<div style={{ height: "calc(100% - 10px)", display: "flex", flexDirection: 'row' }}>
-				<Resizable direction="horizontal">
-					<CodeEditor 
-						initialValue={cell.content}
-						onChange={(value) => updateCell(cell.id, value)}
-					/>
-				</Resizable>
-				{
-					!bundle || bundle.loading ?
-						<div>Loading...</div>
-						:
-						<Preview code={bundle.code} err={bundle.err} />
-				}
-			</div>
-		</Resizable>
-	)
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.content, cell.id, createBundle]);
+
+  return (
+    <Resizable direction="vertical">
+      <div
+        style={{
+          height: 'calc(100% - 1px)',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
+        </Resizable>
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+              <div className="progress-cover">
+                <progress className="progress is-small is-primary" max="100">
+                  Loading
+                </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
+      </div>
+    </Resizable>
+  );
 };
 
 export default CodeCell;
+
